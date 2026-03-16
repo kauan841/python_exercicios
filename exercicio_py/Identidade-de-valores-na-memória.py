629f3b3ad3d003de39d65cd1fa63ddccd2655d88
kauan moraes <kauanmoraesouza@gmail.com>
2026-03-16 18:03:51 -0300

    identidade_memoria.py

---
+++ exercicio_py/Identidade-de-valores-na-memória.py
@@ -0,0 +1,14 @@
+a = 100
+b = 100
+c = 200
+
+print(f'a: {a}')
+print(f'b: {b}')
+print(f'c: {c}')
+
+print(f'id de a: {id(a)}')
+print(f'id de b: {id(b)}')
+print(f'id de c: {id(c)}')
+
+print("a é b?", a is b)
+print("a é c?", a is c)
