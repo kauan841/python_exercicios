606cafbbaf9c541b46feeff58e532cc4721c0e04
kauan moraes <kauanmoraesouza@gmail.com>
2026-03-19 19:06:00 -0300

    validador_senha.py

--- exercicio2_py/Sistema-de-Senha.py
+++ exercicio2_py/Sistema-de-Senha.py
@@ -1,8 +1,15 @@
 senha = input("Digite a senha: ")
-numero = any(char.isdigit() for char in senha)
-letra_maiuscula = any(char.isupper() for char in senha)
 
-if len(senha) >= 8 and numero and letra_maiuscula:
+tem_numero = any(char.isdigit() for char in senha)
+tem_letra = any(char.isalpha() for char in senha)
+tem_maiuscula = any(char.isupper() for char in senha)
+
+if len(senha) >= 8 and tem_numero and tem_letra and tem_maiuscula:
     print("Senha válida!")
 else:
-    print("Senha inválida! A senha deve conter pelo menos 8 caracteres, um número e uma letra maiúscula.")
+    print("Senha inválida!")
+    print("Regras:")
+    print("- Mínimo 8 caracteres")
+    print("- Pelo menos 1 número")
+    print("- Pelo menos 1 letra")
+    print("- Pelo menos 1 letra maiúscula")
