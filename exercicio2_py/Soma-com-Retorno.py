cb1029d2bb5b37b4a9a5f179c174e4f9af5868a1
kauan moraes <kauanmoraesouza@gmail.com>
2026-04-13 16:45:34 -0300

    02_soma.py

---
+++ exercicio2_py/Soma-com-Retorno.py
@@ -0,0 +1,7 @@
+def somar(num1, num2):
+    return num1 + num2
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+print(f"O resultado da soma é: {somar(num1, num2)}")
